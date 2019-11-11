dedba61cc1a554496388ee7ec6007da33ac4a04b
stan-beep <stanleypierrel75@gmail.com>
2019-11-11 17:02:51 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn Design and code react app</h1>
+    <div className="hero">
+      <div className="heroGroup">
+      <h1>Learn Design and code react app</h1>
     <p>Welcome to your new Gatsby site.</p>
-  
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </Layout>
 )
 
