c31f998eb5582d9308d523c5b26dcb34a4f4743c
stan-beep <stanleypierrel75@gmail.com>
2019-11-11 18:08:18 -0500

    Created the header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,18 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from '../images/gatsby-icon.png'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+ <div className="header">
+  <div className="headerGroup">
+    <Link to="/"><img src={logo} width="30" /></Link>
+    <Link to="/courses">Courses</Link>
+    <Link to="/downloads">Downloads</Link>
+    <Link to="/workshops">Courses</Link>
+    <Link to="/buy"><button>Buy</button></Link>
+  </div>
+ </div>
 )
 
 Header.propTypes = {
