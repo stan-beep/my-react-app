7deb0b89401b76dcf9dc8d66788a0262b00f072a
stan-beep <stanleypierrel75@gmail.com>
2019-11-11 18:11:16 -0500

    Update the index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,13 +9,14 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="hero">
       <div className="heroGroup">
-      <h1>Learn Design and code react app</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+        <div className="heroBackground">
+        <h1 className="heroTitle">Learn Design and code react app</h1>
+        <p className="heroParagraph">Complete courses about the best tools and design systems Prototype
+        and build apps with React and swift.</p>
+            <Link className="heroButton" to="/page-2/">watch the video</Link>
       </div>
+        </div>
+     
     </div>
   </Layout>
 )
