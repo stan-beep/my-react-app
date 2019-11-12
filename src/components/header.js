535c19313883ea1d7d9028f9cbf211861dc37ac5
stan-beep <stanleypierrel75@gmail.com>
2019-11-12 10:24:20 -0500

    Change  header.js to a class component for i can change the background of the header style

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,52 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+// import PropTypes from "prop-types"
+import React, { Component } from 'react';
+import "./Header.css"
 import logo from '../images/gatsby-icon.png'
 
-const Header = ({ siteTitle }) => (
- <div className="header">
-  <div className="headerGroup">
-    <Link to="/"><img src={logo} width="30" /></Link>
-    <Link to="/courses">Courses</Link>
-    <Link to="/downloads">Downloads</Link>
-    <Link to="/workshops">Courses</Link>
-    <Link to="/buy"><button>Buy</button></Link>
-  </div>
- </div>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+
+
+
+class Header extends Component {
+  state={
+    hasScrolled:false
+  }
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll=(event)=>{
+    const scrollTop = window.pageYOffset;
+
+    if( scrollTop > 50 ) {
+      this.setState({
+        hasScrolled: true
+      })
+    }
+    else {
+      this.setState({
+        hasScrolled: false
+      })
+    }
+
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'header headerScrolled': 'header'}>
+      <div className="headerGroup">
+        <Link to="/"><img src={logo} width="30" /></Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Courses</Link>
+        <Link to="/buy"><button className="headerButton">Buy</button></Link>
+      </div>
+     </div>
+    );
+  }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+export default Header;
 
-export default Header
+
